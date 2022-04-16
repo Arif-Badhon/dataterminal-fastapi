7a68107800ca053b147786050ba491b82c4e1153
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-04-16 21:13:46 +0600

    updated fast api simple route

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 #import all the modules
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+#create user model
+class User(BaseModel):
+    username: str
+    company: str
+    password: str
+class Login(BaseModel):
+    username: str
+    password: str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 #run the fastapi
 app = FastAPI()

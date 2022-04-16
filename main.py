969d816568b42847d22f1251c59fc260cc0bf19b
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-04-16 23:24:14 +0600

    added basic extra info into signup

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ app.add_middleware(
 class User(BaseModel):
     username: str
     company: str
+    designation: str
+    email: str
     password: str
 class Login(BaseModel):
     username: str
